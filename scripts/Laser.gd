1c1a70467f756d3bf80258c9fd0ab5a41a4a3a6d
Eric Carmichael <eric@ckcollab.com>
2023-03-24 22:38:30 -0700

    make laser work better, start in middle of map

--- scripts/Laser.gd
+++ scripts/Laser.gd
@@ -5,45 +5,51 @@ var is_casting := false setget set_is_casting
 
 
 func _ready() -> void:
-	set_physics_process(false)
-	
-	# Hide line
-	$Line2D.points[0] = Vector2.ZERO
-	$Line2D.points[1] = Vector2.ZERO
+    set_physics_process(false)
+    
+    # Hide line
+    $Line2D.points[0] = Vector2.ZERO
+    $Line2D.points[1] = Vector2.ZERO
 
 func _physics_process(delta: float) -> void:
-	#var cast_point := cast_to
-	force_raycast_update()
-	
-	# TODO: If we want to only display stuff when colliding, use this?
-	#if is_colliding():
-	#	cast_point = to_local(get_collision_point())
-		
-	#var cast_point = get_viewport().get_mouse_position()
-	var cast_point = get_global_mouse_position()
-		
-	$Line2D.points[1] = cast_point
+    #var cast_point := cast_to
+    force_raycast_update()
+    
+    # TODO: If we want to only display stuff when colliding, use this?
+    #if is_colliding():
+    #	cast_point = to_local(get_collision_point())
+        
+    #var cast_point = get_viewport().get_mouse_position()
+    
+    #var cast_point = get_local_mouse_position()		
+    #$Line2D.points[1] = cast_point
+    
+    var cast_point = get_local_mouse_position()
+    var start_point = $Line2D.points[0]
+    var direction = (cast_point - start_point).normalized()
+    var laser_length = 10000  # Choose a large enough value to extend offscreen
+    $Line2D.points[1] = start_point + direction * laser_length
 
 
 func set_is_casting(cast: bool) -> void:
-	is_casting = cast
-	
-	if is_casting:
-		appear()
-	else:
-		disappear()
-		
-	set_physics_process(is_casting)
+    is_casting = cast
+    
+    if is_casting:
+        appear()
+    else:
+        disappear()
+        
+    set_physics_process(is_casting)
 
 
 func appear() -> void:
-	if $Tween.is_active():
-		$Tween.stop_all()
-	$Tween.interpolate_property($Line2D, "width", 0, 10.0, 0.2)
-	$Tween.start()
-	
+    if $Tween.is_active():
+        $Tween.stop_all()
+    $Tween.interpolate_property($Line2D, "width", 0, 10.0, 0.2)
+    $Tween.start()
+    
 func disappear() -> void:
-	if $Tween.is_active():
-		$Tween.stop_all()
-	$Tween.interpolate_property($Line2D, "width", 10.0, 0, 0.2)
-	$Tween.start()
+    if $Tween.is_active():
+        $Tween.stop_all()
+    $Tween.interpolate_property($Line2D, "width", 10.0, 0, 0.2)
+    $Tween.start()
--- scripts/MapGenerator.gd
+++ scripts/MapGenerator.gd
@@ -18,87 +18,92 @@ var grid = []
 #    "floor": 1
 #}
 var Tiles = {
-	"grass_0": 0,
-	"grass_1": 1,
-	"grass_2": 2,
-	"grass_3": 3,
+    "grass_0": 0,
+    "grass_1": 1,
+    "grass_2": 2,
+    "grass_3": 3,
 }
 
 func _init_grid():
-	grid = []
-	for x in width:
-		grid.append([])
-		for y in height:
-			grid[x].append(0);
+    grid = []
+    for x in width:
+        grid.append([])
+        for y in height:
+            grid[x].append(0);
 
 
 func GetRandomDirection():
-	var directions = [[-1, 0], [1, 0], [0, 1], [0, -1]]
-	var direction = directions[rng.randi()%4]
-	return Vector2(direction[0], direction[1])
-	
+    var directions = [[-1, 0], [1, 0], [0, 1], [0, -1]]
+    var direction = directions[rng.randi()%4]
+    return Vector2(direction[0], direction[1])
+    
 func _create_random_path():
-	var max_iterations = 5000
-	var itr = 0
-	
-	var walker = Vector2.ZERO
-	
-	while itr < max_iterations:
-		
-		# Perform random walk
-		# 1- choose random direction
-		# 2- check that direction is in bounds
-		# 3- move in that direction
-		var random_direction = GetRandomDirection()
-		
-		if (walker.x + random_direction.x >= 0 and 
-			walker.x + random_direction.x < width and
-			walker.y + random_direction.y >= 0 and
-			walker.y + random_direction.y < height):
-				
-				walker += random_direction
-				var choices = [0, 1, 2, 3]
-				var tile = choices[rng.randi()%3]
-				match(tile):
-					0:
-						grid[walker.x][walker.y] = Tiles.grass_1
-					1:
-						grid[walker.x][walker.y] = Tiles.grass_2
-					2:
-						grid[walker.x][walker.y] = Tiles.grass_3
+    var max_iterations = 5000
+    var itr = 0
+    
+    var walker = Vector2.ZERO
+    
+    while itr < max_iterations:
+        
+        # Perform random walk
+        # 1- choose random direction
+        # 2- check that direction is in bounds
+        # 3- move in that direction
+        var random_direction = GetRandomDirection()
+        
+        if (walker.x + random_direction.x >= 0 and 
+            walker.x + random_direction.x < width and
+            walker.y + random_direction.y >= 0 and
+            walker.y + random_direction.y < height):
+                
+                walker += random_direction
+                var choices = [0, 1, 2, 3]
+                var tile = choices[rng.randi()%3]
+                match(tile):
+                    0:
+                        grid[walker.x][walker.y] = Tiles.grass_1
+                    1:
+                        grid[walker.x][walker.y] = Tiles.grass_2
+                    2:
+                        grid[walker.x][walker.y] = Tiles.grass_3
 #				grid[walker.x][walker.y] = Tiles.grass_1
-				itr += 1
-	
+                itr += 1
+    
 func _spawn_tiles():
-	for x in width:
-		for y in height:
-			if x == 0 or x == width - 1:
-				rock_tilemap.set_cellv(Vector2(x, y), 1)
-			elif y == 0 or y == height - 1:
-				rock_tilemap.set_cellv(Vector2(x, y), 0)
-			
-			match grid[x][y]:
-				Tiles.grass_0:
-					grass_tilemap.set_cellv(Vector2(x, y), 0)
-				Tiles.grass_1:
-					grass_tilemap.set_cellv(Vector2(x, y), 1)
-				Tiles.grass_2:
-					grass_tilemap.set_cellv(Vector2(x, y), 2)
-				Tiles.grass_3:
-					grass_tilemap.set_cellv(Vector2(x, y), 3)
+    for x in width:
+        for y in height:
+            if x == 0 or x == width - 1:
+                rock_tilemap.set_cellv(Vector2(x, y), 1)
+            elif y == 0 or y == height - 1:
+                rock_tilemap.set_cellv(Vector2(x, y), 0)
+            
+            match grid[x][y]:
+                Tiles.grass_0:
+                    grass_tilemap.set_cellv(Vector2(x, y), 0)
+                Tiles.grass_1:
+                    grass_tilemap.set_cellv(Vector2(x, y), 1)
+                Tiles.grass_2:
+                    grass_tilemap.set_cellv(Vector2(x, y), 2)
+                Tiles.grass_3:
+                    grass_tilemap.set_cellv(Vector2(x, y), 3)
 
 
 func _clear_tilemaps():
-	for x in width:
-		for y in height:
-			grass_tilemap.clear()
+    for x in width:
+        for y in height:
+            grass_tilemap.clear()
   
-	grass_tilemap.update_bitmask_region()
+    grass_tilemap.update_bitmask_region()
+    
+func _place_player():
+    # Put our guy in the center
+    $"../Player".position = Vector2(width / 2, height / 2) * CellSize
 
 func _ready():
-	rng.randomize()
-	_init_grid()
-	_clear_tilemaps()
-	_create_random_path()
-	_spawn_tiles()
+    rng.randomize()
+    _init_grid()
+    _clear_tilemaps()
+    _create_random_path()
+    _spawn_tiles()
+    _place_player()
 
